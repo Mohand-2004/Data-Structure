6db3cb6e6fa67bd051d177159fc78569654758f5
mohand <mohandelsebaey@gmail.com>
2024-05-21 16:31:22 +0300

    Done !

--- C++/stack.cpp
+++ C++/stack.cpp
@@ -74,14 +74,3 @@ template <typename DataType> class Stack{
             return this->linkedlist[index];
         }
 };
-
-int main(void){
-    Stack<int> stack;
-    for(int i = 1;i<6;i++){
-        stack.push(i);
-    }
-
-    for(int i = 0;i<stack.lenght();i++){
-        cout<<stack[i]<<' ';
-    }
-}
